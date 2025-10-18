af5381f9e4f9ed129fd4b952df5a065cf277d9d8
dinhmv <dinhmv@live.com>
2025-10-18 15:38:05 +0700

    Update API base URL for production environment

--- src/controllers/config.ts
+++ src/controllers/config.ts
@@ -1,6 +1,6 @@
 const baseUrl = window.location.origin.includes('localhost')
 ? 'http://localhost:8888/api'
-: 'https://bravo.indiebold.com/api'; //change this to your server URL
+: 'https://shpvideo.netlify.app/api'; //change this to your server URL
 
 
 
